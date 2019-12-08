bc9bce30933131fe8ef4d7103027b4bd79454028
Rodrigo Anderson dos Santos <rodrigo.ast@gmail.com>
2019-12-08 13:07:49 -0300

    Cria a classe ClienteContext e faz migrations

---
+++ src/SGC.Infra/Data/ClienteContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.Application.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Infra.Data
+{
+    public class ClienteContext: DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+   
+        }
+    }
+}
--- src/SGC.UI.Web/Startup.cs
+++ src/SGC.UI.Web/Startup.cs
@@ -12,6 +12,7 @@ using SGC.UI.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SGC.Infra.Data;
 
 namespace SGC.UI.Web
 {
@@ -30,10 +31,15 @@ namespace SGC.UI.Web
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddDbContext<ClienteContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
